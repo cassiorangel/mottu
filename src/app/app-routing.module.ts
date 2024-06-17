acc2403adb702b3a7a8829ee469a35c605b74d86
Cassio Fernandes <cassio.rfernandes@gmail.com>
2024-06-17 15:16:42 -0300

    ajuste form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    loadChildren: () => import('./aplication/aplication.module').then(m => m.AplicationModule)
+  },
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
