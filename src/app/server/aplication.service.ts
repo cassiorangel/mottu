6d9658eb4015b5271adc99fc926e03670c6bbc7f
Cassio Fernandes <cassio.rfernandes@gmail.com>
2024-06-18 12:26:08 -0300

    css header

--- src/app/server/aplication.service.ts
+++ src/app/server/aplication.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { map } from 'rxjs';
+
+import { map, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AplicationService {
+
+  public stateArr$ = new BehaviorSubject<any>([]);
 
   constructor(
     private http: HttpClient
@@ -17,5 +20,19 @@ export class AplicationService {
       .pipe(
         map((res: any) => res['results'])
       )
+  }
+
+  get currentState() {
+    return this.stateArr$.getValue();
+  }
+
+  setState(newState: any) {
+    let oldState = this.currentState;
+    console.log('old', oldState);
+    console.log('new', newState);
+
+    if (JSON.stringify(oldState) !== JSON.stringify(newState)) {
+      this.stateArr$.next(newState);
+    }
   }
 }
